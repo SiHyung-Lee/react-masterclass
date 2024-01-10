7bc88dd36315d90863d5782cc9308a487b689f64
dev-athome <l_sh@athomecorp.com>
2024-01-10 16:59:14 +0900

    ..

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -31,11 +31,35 @@ interface RouteState {
     };
 }
 
+interface InfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    logo: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface priceData {}
+
 function Coin() {
     const [loading, setLoading] = useState(true);
     const { coinId } = useParams<{ coinId: string }>();
     const { state } = useLocation() as RouteState;
-    const [info, setInfo] = useState({});
+    const [info, setInfo] = useState<InfoData>();
     const [priceInfo, setPriceInfo] = useState({});
 
     useEffect(() => {
@@ -46,6 +70,8 @@ function Coin() {
             const priceData = await (
                 await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
             ).json();
+            console.log(infoData);
+            console.log(priceData);
             setInfo(infoData);
             setPriceInfo(priceData);
         })();
