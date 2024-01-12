061ae87e8c8c3868c6020708571a5ed077d33a6f
dev-athome <l_sh@athomecorp.com>
2024-01-12 20:52:24 +0900

    #5.6 Data Types

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -53,14 +53,44 @@ interface InfoData {
     last_data_at: string;
 }
 
-interface priceData {}
+interface PriceData {
+    id: 'string';
+    name: 'string';
+    symbol: 'string';
+    rank: 'number';
+    circulating_supply: 'number';
+    total_supply: 'number';
+    max_supply: 'number';
+    beta_value: 'number';
+    first_data_at: 'string';
+    last_updated: 'string';
+    quotes: {
+        ath_date: string;
+        ath_price: number;
+        market_cap: number;
+        market_cap_change_24h: number;
+        percent_change_1h: number;
+        percent_change_1y: number;
+        percent_change_6h: number;
+        percent_change_7d: number;
+        percent_change_12h: number;
+        percent_change_15m: number;
+        percent_change_24h: number;
+        percent_change_30d: number;
+        percent_change_30m: number;
+        percent_from_price_ath: number;
+        price: number;
+        volume_24h: number;
+        volume_24h_change_24h: number;
+    };
+}
 
 function Coin() {
     const [loading, setLoading] = useState(true);
     const { coinId } = useParams<{ coinId: string }>();
     const { state } = useLocation() as RouteState;
     const [info, setInfo] = useState<InfoData>();
-    const [priceInfo, setPriceInfo] = useState({});
+    const [priceInfo, setPriceInfo] = useState<PriceData>();
 
     useEffect(() => {
         (async () => {
@@ -70,8 +100,6 @@ function Coin() {
             const priceData = await (
                 await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
             ).json();
-            console.log(infoData);
-            console.log(priceData);
             setInfo(infoData);
             setPriceInfo(priceData);
         })();
