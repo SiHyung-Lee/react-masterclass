6b5575c9bc65a5daace569fe0388c01a7ccf8622
dev-athome <l_sh@athomecorp.com>
2024-10-02 21:04:24 +0900

    테마 설정 추가
    styled-components의 ThemeProvider를 이용해 다크 테마를 추가했습니다. 기존 불필요한 애니메이션과 컴포넌트를 제거하고, 간단한 "Hello World" 타이틀을 표시하도록 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -6,47 +10,13 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-      transform: rotate(0deg);
-      border-radius: 0;
-  }
-  50% {
-      border-radius: 100px;
-  }
-  100% {
-      transform: rotate(360deg);
-      border-radius: 0;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotationAnimation} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  ${Emoji}:hover {
-      font-size: 98px;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>🤪</Emoji>
-      </Box>
-      <Emoji>🤪</Emoji>
+      <Title>Hello World</Title>
     </Wrapper>
   );
 }
