c78661db71b34f7643a64ecef9967bf28146bcad
dev-athome <l_sh@athomecorp.com>
2024-10-02 19:44:07 +0900

    애니메이션 및 이모지 추가
    Box 컴포넌트에 회전 애니메이션을 추가하고, 클릭 시 이모지 크기가 커지는 기능을 구현했습니다. Father를 Wrapper로 변경하고 레이아웃을 중앙 정렬했습니다.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
-`;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
+const rotationAnimation = keyframes`
+  0% {
+      transform: rotate(0deg);
+      border-radius: 0;
+  }
+  50% {
+      border-radius: 100px;
+  }
+  100% {
+      transform: rotate(360deg);
+      border-radius: 0;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${Emoji}:hover {
+      font-size: 98px;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji>🤪</Emoji>
+      </Box>
+      <Emoji>🤪</Emoji>
+    </Wrapper>
   );
 }
 
