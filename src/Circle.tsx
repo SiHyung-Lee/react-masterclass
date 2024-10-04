2d9825d407f61770c0e65f8b1121c9906a92fba7
dev-athome <l_sh@athomecorp.com>
2024-10-04 16:56:38 +0900

    스타일 도구 제거 및 Circle 컴포넌트 추가
    styled-components를 제거하고 Circle 컴포넌트를 도입했습니다. App 컴포넌트 내에 Circle 인스턴스를 추가하고, ThemeProvider를 제거했습니다. Circle 컴포넌트는 동적으로 배경색을 설정할 수 있습니다.

---
+++ src/Circle.tsx
@@ -0,0 +1,8 @@
+interface CircleProps {
+  bgColor: string;
+}
+function Circle({ bgColor }: CircleProps) {
+  return <div style={{ backgroundColor: bgColor }}>sdafdsfsdfdsa</div>;
+}
+
+export default Circle;
