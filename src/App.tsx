f39c379f79de2c0cdd88449c870e21ff7731da04
SiHyung-Lee <buzzcatfish@gmail.com>
2024-10-20 23:40:42 +0900

    Circle 컴포넌트 제거 및 로그인 폼 추가
    Circle 컴포넌트를 제거하고, App 컴포넌트에 로그인 폼을 추가했습니다. 업데이트된 폼은 입력값을 상태로 관리하고 제출 시 콘솔에 로그를 출력합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" borderColor="black" />
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} type="text" placeholder="username" />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
