663abf93440a267d80f6ac5029c8968352e3b88e
dev-athome <l_sh@athomecorp.com>
2024-11-06 20:09:42 +0900

    추가: react-router-dom 타입 선언 및 관련 패키지 업데이트
    react-router-dom에 대한 타입 선언 파일을 추가했습니다. tsconfig.json에 타입 루트를 정의하고, package-lock.json에서 관련 패키지들을 업데이트했습니다.

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,12 @@
+import { useParams } from "react-router";
+
+interface RouteParams {
+  coinId: string;
+}
+
 function Coin() {
+  const { coinId } = useParams<RouteParams>();
+  console.log(coinId);
   return <h1>Coin</h1>;
 }
 
