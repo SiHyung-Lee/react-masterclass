ec248f00c751bcc6e8894b7fed91e76ef73b32a7
SiHyung-Lee <buzzcatfish@gmail.com>
2024-11-18 23:55:02 +0900

    타입 추가 및 코드 수정
    IHistorical 인터페이스를 추가하여 데이터를 타입 안전하게 처리했습니다. useQuery 훅을 사용할 때 제네릭 화살표를 추가하여 올바른 데이터 타입을 명시했습니다.

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,11 +1,21 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
 
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 interface ChartProps {
   coinId: string;
 }
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId),
   );
   return <h1>Chart</h1>;
